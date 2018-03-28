8195e89eea87baead068856d939f48c5b9e23df8
Dead4Now <joshuafielding1@gmail.com>
2018-03-28 11:26:48 +0100

    Initial commit

---
+++ simplequizbot.py
@@ -0,0 +1,49 @@
+score = 0
+print("Welcome to this multiple answer customizable quiz!")
+# The first question will be below
+print("Put question 1 here")
+answer = input()
+answer = str(answer)
+if answer == "Correct Answer here":
+    print("Correct!") # You can change this reponse
+    score = score + 1
+else:
+    print("That's wrong, you failed") # You can change this reponse
+    score = score - 1
+# second question
+print("Put question 2 here")
+answer = input()
+answer = str(answer)
+if answer == "Correct Answer here":
+    print("Correct!") # You can change this reponse
+    score = score + 1
+else:
+    print("That's wrong, you failed") # You can change this reponse
+    score = score - 1
+# third question
+print("Put question 2 here")
+answer = input()
+answer = str(answer)
+if answer == "Correct Answer here":
+    print("Correct!") # You can change this reponse
+    score = score + 1
+else:
+    print("That's wrong, you failed") # You can change this reponse
+    score = score - 1
+# fourth question
+print("Put question 2 here")
+answer = input()
+answer = str(answer)
+if answer == "Correct Answer here":
+    print("Correct!") # You can change this reponse
+    score = score + 1
+else:
+    print("That's wrong, you failed") # You can change this reponse
+    score = score - 1
+
+# Paste any other questions before this line
+# We will now tell the user there results on this quiz
+# Here we go:
+
+# I completely forgot what to do - I've got it on a sheet for my test somewhere
+# I'll get it updated asap
